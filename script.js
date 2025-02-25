7a0ce15015f16876df168830ded1dc8f2c52facc
AaQuaS <np.giraffespots@gmail.com>
2025-02-24 21:26:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-$.ajax({url:'https://api.ipify.org/?format=json',sucess:function(data){document.getElementById('ipaddress').innerHTML=data.ip;}});
+$.ajax({url:'https://api.ipify.org/?format=json',sucess:function(data){document.getElementById('ipadress').innerHTML=data.ip;}});
